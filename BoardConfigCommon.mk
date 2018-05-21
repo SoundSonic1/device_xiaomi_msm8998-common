70af3a53f2af0aa23b803b6a7a1337edd71b722d
Kien Dang Tran <kiendangtran0@gmail.com>
2018-05-21 12:06:31 +0200

    config: change kernel compiler

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -43,7 +43,8 @@ BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_RAMDISK_OFFSET := 0x01000000
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+KERNEL_TOOLCHAIN := /home/kien/Android/LOS/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android/bin
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-opt-linux-android-
 TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8998
 
 BOARD_KERNEL_CMDLINE := androidboot.console=ttyMSM0 androidboot.hardware=qcom
